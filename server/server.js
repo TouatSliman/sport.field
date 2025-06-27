549ab58904ff8ff3b14260f69ee4ac4cc5dd58ad
mustapha <ssoverlord04@gmail.com>
2025-06-28 00:54:31 +0100

    remove access to all origins

--- server/server.js
+++ server/server.js
@@ -24,10 +24,13 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // allow origin
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
+  res.header(
+    "Access-Control-Allow-Origin",
+    process.env.CLIENT_URL || "http://localhost:5173"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
 //apply arcjet middleware
